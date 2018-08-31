1862169528d3aea6dd9f9ee03e025657ec19f170
Yuxiang Li <li.yuxiang.nj@gmail.com>
2018-08-31 19:12:00 +0200

    Resume the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { MapService } from './map/map.service';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
-    SearchComponent
+    MapComponent
   ],
   imports: [
     BrowserModule,
