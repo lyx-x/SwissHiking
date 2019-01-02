a0e16e8834946529a7bff272e6905adea0cd4640
Yuxiang Li <li.yuxiang.nj@gmail.com>
2019-01-02 21:17:00 +0100

    Added header check for admin API so it can only be called from a GAE Cron job

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -11,38 +11,44 @@ const dataBuffer = Buffer.from('');
 
 /* Update the list of tracks from SchweizMobil. */
 router.get('/update/schweizmobil/tracklist', function (req, res, next) {
-  console.log(req.headers);
+  if (req.headers['x-appengine-cron']) {
+    // Add two custom attributes to the message
+    const customAttributes = {
+      source: 'SchweizMobil',
+      update_list: 'true',
+    };
 
-  // Add two custom attributes to the message
-  const customAttributes = {
-    source: 'SchweizMobil',
-    update_list: 'true',
-  };
-
-  pubsub.topic(topicName)
-    .publisher()
-    .publish(dataBuffer, customAttributes)
-    .then((messageId) => console.log(`Message ${messageId} published.`));
-  
-  res.send('Track coordinates updated.')
+    pubsub.topic(topicName)
+      .publisher()
+      .publish(dataBuffer, customAttributes)
+      .then((messageId) => console.log(`Message ${messageId} published.`));
+    
+    res.send('Track coordinates updated.');
+  } else {
+    res.status(403);
+    res.send('Admin API is limited to Cron job.');
+  }
 });
 
 /* Update one track info from SchweizMobil. */
 router.get('/update/schweizmobil/track', function (req, res, next) {
-  console.log(req.headers);
-  
-  // Add two custom attributes to the message
-  const customAttributes = {
-    source: 'SchweizMobil',
-    update_track: 'true',
-  };
+  if (req.headers['x-appengine-cron']) {
+    // Add two custom attributes to the message
+    const customAttributes = {
+      source: 'SchweizMobil',
+      update_track: 'true',
+    };
 
-  pubsub.topic(topicName)
-    .publisher()
-    .publish(dataBuffer, customAttributes)
-    .then((messageId) => console.log(`Message ${messageId} published.`));
+    pubsub.topic(topicName)
+      .publisher()
+      .publish(dataBuffer, customAttributes)
+      .then((messageId) => console.log(`Message ${messageId} published.`));
 
-  res.send('Track coordinates updated.')
+    res.send('Track coordinates updated.')
+  } else {
+    res.status(403);
+    res.send('Admin API is limited to Cron job.');
+  }
 });
 
 /* Cleanup old data. */
